c9a3a0e0105ca9f2770b63d0a89e202be3e3e21e
olvsivkov <1000listov@mail.ru>
2024-01-27 13:29:38 +0300

    build progect

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,3 +1,27 @@
+import { BrowserRouter as Routes, Route } from 'react-router-dom';
+import { setupListeners } from '@reduxjs/toolkit/query';
+import { Provider } from 'react-redux';
+import { setupStore } from '../shared/redux-query-api';
+import { PostContainer } from '../pages';
+import { PostPage } from '../pages';
+
+const store = setupStore();
+setupListeners(store.dispatch);
+
+const App = () => {
+  return (
+    <Provider store={store}>
+        <Routes>
+          <Route path='/' element={<PostContainer />} />
+          <Route path='/posts/:id' element={<PostPage />} />
+        </Routes>
+    </Provider>
+  );
+};
+
+export default App;
+
+/**
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { setupListeners } from '@reduxjs/toolkit/query';
 import { Provider } from 'react-redux';
@@ -20,31 +44,4 @@ const App = () => {
     </Provider>
   );
 };
-
-export default App;
-
-
-/*import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { setupListeners } from '@reduxjs/toolkit/query';
-import { Provider } from 'react-redux';
-import { setupStore } from '../shared/redux-query-api';
-import { PostContainer } from '../pages';
-import { PostPage } from '../pages';
-
-const store = setupStore();
-setupListeners(store.dispatch);
-
-const App = () => {
-  return (
-    <Provider store={store}>
-      <Router>
-        <Routes>
-          <Route path='/' element={<PostContainer />} />
-          <Route path='/posts/:id' element={<PostPage />} />
-        </Routes>
-      </Router>
-    </Provider>
-  );
-};
-
-export default App;*/
+ */
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,21 @@
-import ReactDOM from 'react-dom/client';
 
+import ReactDOM from 'react-dom/client';
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import './index.css';
 import App from './app/App';
+
+const router = createHashRouter([
+  {
+    path:'/*',
+    element: <App />
+  }
+])
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <App />
+  <RouterProvider router={router}/>
 );
+
+
