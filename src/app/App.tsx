36bef56eb19c824cf816c71f06109b05edb0c5d2
olvsivkov <1000listov@mail.ru>
2024-01-27 12:54:00 +0300

    add hashКouter to progect for GH-pages deploy

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,3 @@
-// App.tsx
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { setupListeners } from '@reduxjs/toolkit/query';
 import { Provider } from 'react-redux';
@@ -23,3 +22,29 @@ const App = () => {
 };
 
 export default App;
+
+
+/*import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { setupListeners } from '@reduxjs/toolkit/query';
+import { Provider } from 'react-redux';
+import { setupStore } from '../shared/redux-query-api';
+import { PostContainer } from '../pages';
+import { PostPage } from '../pages';
+
+const store = setupStore();
+setupListeners(store.dispatch);
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route path='/' element={<PostContainer />} />
+          <Route path='/posts/:id' element={<PostPage />} />
+        </Routes>
+      </Router>
+    </Provider>
+  );
+};
+
+export default App;*/
